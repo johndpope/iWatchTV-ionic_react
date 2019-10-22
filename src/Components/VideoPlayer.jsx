0a5f3360cd529b2e1f8218b4e59cc05d9b2f1f41
migtamrod <admin@aiotvp.com>
2019-10-21 23:06:47 -0400

    6th commit

--- src/Components/VideoPlayer.jsx
+++ src/Components/VideoPlayer.jsx
@@ -7,23 +7,23 @@ export const VideoPlayer = (props) => {
     const [stream, setStream] = React.useState(props.stream);
 
     const handleClick = (click) => {
-        stream === 0 ? setStream(1) : stream === 1 ? setStream(2) : setStream(0)
+        stream === 1 ? setStream(2) : setStream(1)
     }
 
     return (
         <div style={{ height: 300, width: '100%', marginBottom: 40 }}>
-                    <IonCardSubtitle>Movie Player</IonCardSubtitle>
-                    <IonButtons>
-                    <IonButton onClick={handleClick}>Change Player</IonButton>
-                    </IonButtons>
-                    {stream === 1 ?
-    <iframe title={imdb_id} src={`https://database.gdriveplayer.us/player.php?imdb=${imdb_id}`} height={'300px'} width={'100%'} frameBorder='0' />
-    :
-    stream === 2 ?
-        <iframe title={imdb_id} src={`https://gomostream.com/movie/${imdb_id}`} height={'300px'} width={'100%'} frameBorder='0' loading="lazy" allowFullScreen={true}/>
-        :
-        <iframe title={imdb_id} src={`http://movie2konline.net/api/openload.php?id=${imdb_id}`} height={'300px'} width={'100%'} frameBorder='0' loading="lazy" allowFullScreen={true} sandbox="allow-scripts allow-same-origin" />
-}
+            <IonCardSubtitle>Movie Player</IonCardSubtitle>
+            <IonButtons>
+                <IonButton onClick={handleClick}>Change Player</IonButton>
+            </IonButtons>
+            {stream === 1 ?
+                <iframe title={imdb_id} src={`https://database.gdriveplayer.us/player.php?imdb=${imdb_id}`} height={'300px'} width={'100%'} frameBorder='0' />
+                :
+                stream === 2 ?
+                    <iframe title={imdb_id} src={`https://gomostream.com/movie/${imdb_id}`} height={'300px'} width={'100%'} frameBorder='0' loading="lazy" allowFullScreen={true} />
+                    :
+                    <iframe title={imdb_id} src={`http://movie2konline.net/api/openload.php?id=${imdb_id}`} height={'300px'} width={'100%'} frameBorder='0' loading="lazy" allowFullScreen={true} sandbox="allow-scripts allow-same-origin" />
+            }
         </div>
     )
 }
